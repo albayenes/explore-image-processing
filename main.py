a5ee85dfa34b99d43c330bd9088a317071a0a3ec
Enes Albay <albayenes@gmail.com>
2021-10-22 12:26:12 +0300

    Menu actions added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QAction
+from PyQt5.QtGui import QIcon
 import sys
 
 
@@ -8,22 +9,48 @@ class MainWindow(QMainWindow):
         self.setGeometry(200, 200, 500, 300)
         self.setWindowTitle("Experimental Image Processing Tool")
 
+        self._createActions()
         self._createMenuBar()
         self.initUI()
 
     def initUI(self):
         pass
 
+    def _createActions(self):
+        # Creating action using the first constructor
+        self.newAction = QAction(self)
+        self.newAction.setText("&New")
+        # Creating actions using the second constructor
+        self.openAction = QAction("&Open...", self)
+        self.saveAction = QAction("&Save", self)
+        self.exitAction = QAction("&Exit", self)
+        self.copyAction = QAction("&Copy", self)
+        self.pasteAction = QAction("&Paste", self)
+        self.cutAction = QAction("C&ut", self)
+        self.helpContentAction = QAction("&Help Content", self)
+        self.aboutAction = QAction("&About", self)
+
     def _createMenuBar(self):
-        menu_bar = self.menuBar()
+        menuBar = self.menuBar()
 
-        file_menu = QMenu("&File", self)
-        edit_menu = QMenu("&Edit", self)
-        help_menu = QMenu("&Help", self)
+        fileMenu = QMenu("&File", self)
+        editMenu = QMenu("&Edit", self)
+        helpMenu = QMenu("&Help", self)
 
-        menu_bar.addMenu(file_menu)
-        menu_bar.addMenu(edit_menu)
-        menu_bar.addMenu(help_menu)
+        menuBar.addMenu(fileMenu)
+        fileMenu.addAction(self.newAction)
+        fileMenu.addAction(self.openAction)
+        fileMenu.addAction(self.saveAction)
+        fileMenu.addAction(self.exitAction)
+
+        menuBar.addMenu(editMenu)
+        editMenu.addAction(self.copyAction)
+        editMenu.addAction(self.pasteAction)
+        editMenu.addAction(self.cutAction)
+
+        menuBar.addMenu(helpMenu)
+        helpMenu.addAction(self.helpContentAction)
+        helpMenu.addAction(self.aboutAction)
 
 
 def window():
